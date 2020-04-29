5a96625763d14f1927bbbb623f996c82c84e0246
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-29 14:15:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,6 +24,14 @@ class TicTacToe
   def move(index, token = "X")
     @board[index] = token
   end
+  
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(index)
+    index.between(0,8) && !position_taken?
+  end
     
 end
 
